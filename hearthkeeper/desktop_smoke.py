e720d9fc88e6a2021f50ce2a4102c36147f6759e
AbbyUsesAIThatCodes <abbyusesaithatcodes@gmail.com>
2026-09-15 21:31:05 -0400

    Wrap home shortcuts for Windows font metrics

--- hearthkeeper/desktop_smoke.py
+++ hearthkeeper/desktop_smoke.py
@@ -165,7 +165,7 @@ def exercise_home(application, window, directory):
             window.grab().save(str(directory / "desktop-home-overflow.png"))
             details = [(type(item).__name__, item.objectName(), item.minimumSizeHint().width(), item.width(), getattr(item, "text", lambda: "")()[:100])
                        for item in scroll.widget().findChildren(QWidget) if item.minimumSizeHint().width() > scroll.viewport().width() - 100]
-            raise AssertionError("Home overflows horizontally: " + str((scroll.viewport().width(), scroll.widget().width(), details)))
+            raise AssertionError("Home overflows horizontally: " + str((scroll.viewport().width(), scroll.widget().width(), [(i, scroll.widget().layout().itemAt(i).minimumSize().width()) for i in range(scroll.widget().layout().count())], details)))
         scroll.ensureWidgetVisible(window.play_button); application.processEvents()
         window.grab().save(str(directory / "desktop-home-small.png"))
         process = Mock(); process.poll.return_value = None
